800c37ac41e9bd95ff72e1e54c05f9a86144e712
Shahriyor <baxtiyarovich0102@gmail.com>
2025-04-30 16:10:31 +0500

    get methods are ready

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private courses: {id: string, title: string, desc: string, price: number, teacherName: string, isAcrive?: boolean}[] = [
+    {
+      id: "1",
+      title: "Birinchi",
+      desc: "Xaaa",
+      price: 100,
+      teacherName: "Men"
+    },
+    {
+      id: "2",
+      title: "Ikkinchi",
+      desc: "Xaaa",
+      price: 100,
+      teacherName: "Men"
+    }
+  ]
+
+  getAllCourses(){
+    return this.courses
   }
+
+  getCourseById(id:string): any{
+    return this.courses.filter((course) => course.id == id).length
+    ? this.courses.filter((course) => course.id == id)[0]
+    : null
+  }
+
+  
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {VersioningType} from "@nestjs/common"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  // Enable versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
